aa584c6a59809a76c34c9b13271b78d2b1161118
HamdiAyhan <hamdiminer1@gmail.com>
2024-08-31 13:36:31 +0300

    yorumlar eklendi

--- src/Review.js
+++ src/Review.js
@@ -1,9 +1,13 @@
 import React, { useState } from 'react';
 import people from './data';
 import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
+
+  
 const Review = () => {
   const [index, setIndex] = useState(0);
   const { name, job, image, text } = people[index];
+
+  //* burada gezinme fonksiyonlarından gelen değerler kontrol edilip son elemandan sonra  en başa, ilk elemandan sonra  en sona dönmesi sağlanmış index people dan büyükse index i 0 olarak render et
   const checkNumber = (number) => {
     if (number > people.length - 1) {
       return 0;
@@ -13,18 +17,21 @@ const Review = () => {
     }
     return number;
   };
+  //* index değerini bir artırarak değeri checkNumber fonksiyonuna göndermiş
   const nextPerson = () => {
     setIndex((index) => {
       let newIndex = index + 1;
       return checkNumber(newIndex);
     });
   };
+  //* index değerini bir azaltarak checkNumber fonksiyonuna göndermiş
   const prevPerson = () => {
     setIndex((index) => {
       let newIndex = index - 1;
       return checkNumber(newIndex);
     });
   };
+  //* suprise me butonunda kullanılmak üzere rastgele bir sayı üretilimiş ve index değişkenine ve checkNumber fonksiyonuna değer olarak yollanmış 
   const randomPerson = () => {
     let randomNumber = Math.floor(Math.random() * people.length);
     if (randomNumber === index) {
